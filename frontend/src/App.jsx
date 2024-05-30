f1d6bbece122a8a45dd47006fc6f7be5d3ce5349
Koffi GREBE <yvan.grebe@email.com>
2024-05-30 01:32:13 +0100

    affichage footer

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,6 +1,9 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Header from "./components/Header.jsx";
 import Home from "./pages/Home.jsx";
+import Category from "./pages/Category.jsx";
+import Product from "./pages/Product.jsx";
+import Footer from "./components/Footer.jsx";
 
 export default function App() {
   return (
@@ -9,7 +12,15 @@ export default function App() {
         <Header />
         <Routes>
           <Route path="/" element={<Home />} />
+          <Route path="/clothing" element={<Category />} />
+          <Route path="/cosmetics" element={<Category />} />
+          <Route path="/electronics" element={<Category />} />
+          <Route path="/product" element={<Category />} />
+          <Route path=":productId" element={<Product />}></Route>
+          {/* <Route path="/cart-page" element={<Cart />} />
+          <Route path="/login" element={<Login />} /> */}
         </Routes>
+        <Footer />
       </BrowserRouter>
     </main>
   );
