5f0ce5492421d6f9ee05747fe5a76019e9f9a869
Koffi GREBE <yvan.grebe@email.com>
2024-05-30 07:37:56 +0100

    affichage logo Header.jsx

--- frontend/src/components/Header.jsx
+++ frontend/src/components/Header.jsx
@@ -1,7 +1,20 @@
 import React from "react";
+import { Link } from "react-router-dom";
+// import images / svgs
+import logo from "../assets/logo.png";
+import user from "../assets/user.svg";
 
 const Header = () => {
-  return <div>Header</div>;
+  return (
+    <header>
+      <div>
+        {/* {logo} */}
+        <Link to={"/"}>
+          <img src={logo} alt="logoImg" height={31} width={31} />
+        </Link>
+      </div>
+    </header>
+  );
 };
 
 export default Header;
