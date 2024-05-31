49e36c26b8a782ff77f902a6a2574aef30a0e0d1
Koffi GREBE <yvan.grebe@email.com>
2024-05-31 01:09:24 +0100

    Add logo navbar and buttons CSS 45

--- frontend/src/components/Header.jsx
+++ frontend/src/components/Header.jsx
@@ -3,15 +3,30 @@ import { Link } from "react-router-dom";
 // import images / svgs
 import logo from "../assets/logo.png";
 import user from "../assets/user.svg";
+import Navbar from "./Navbar";
 
 const Header = () => {
   return (
-    <header>
-      <div>
+    <header className="max-padd-container w-full z-50">
+      <div className="flexBetween py-3">
         {/* {logo} */}
-        <Link to={"/"}>
+        <Link to={"/"} className="flex items-center gap-x-2">
           <img src={logo} alt="logoImg" height={31} width={31} />
+          <span className="bold-24 hidden xs:flex">Merchanza</span>
         </Link>
+        {/* {Navbar and buttons} */}
+        <div className="flexCenter gap-x-4">
+          {/* {Desktop Navbar } */}
+          <div>
+            <Navbar
+              containerStyles={
+                "hidden xl:flex gap-x-5 xl:gap-x-10 medium-15 rounded-full px-2 py-1"
+              }
+            />
+          </div>
+          {/* {buttons} */}
+          <div>buttons</div>
+        </div>
       </div>
     </header>
   );
