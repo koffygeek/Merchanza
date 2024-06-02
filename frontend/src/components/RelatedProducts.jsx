464f3746bb203089b0c6697751b70d7c66b7452d
Koffi GREBE <yvan.grebe@email.com>
2024-06-02 05:57:31 +0100

    affichage RelatedProducts.jsx

--- frontend/src/components/RelatedProducts.jsx
+++ frontend/src/components/RelatedProducts.jsx
@@ -1,7 +1,59 @@
 import React from "react";
+import LATEST from "../assets/latest";
+import { Link } from "react-router-dom";
+// import Swiper React components
+import { Swiper, SwiperSlide } from "swiper/react";
+// import Swiper styles
+import "swiper/css";
+import { RiShoppingCart2Line } from "react-icons/ri";
 
 const RelatedProducts = () => {
-  return <div>RelatedProducts</div>;
+  return (
+    <div>
+      <h4 className="border-l-4 pl-2 border- bold-20">Related Products</h4>
+      {/* {container} */}
+      <div className="mx-auto max-w-full">
+        <Swiper
+          breakpoints={{
+            600: {
+              slidesPerView: 2,
+              spaceBetween: 30,
+            },
+            1024: {
+              slidesPerView: 3,
+              spaceBetween: 30,
+            },
+            1200: {
+              slidesPerView: 4,
+              spaceBetween: 30,
+            },
+          }}
+          className="h-[420px] sm:h-[477px] mt-5"
+        >
+          {LATEST.map((item, i) => (
+            <SwiperSlide key={i}>
+              <Link to={""}>
+                <img src={item.image} alt="" />
+                <div>
+                  <h4>{item.name}</h4>
+                  <p>
+                    Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                  </p>
+                  <div>
+                    <div>
+                      <span>${item.new_price}.00</span>
+                      <span> ${item.old_price}.00</span>
+                    </div>
+                    <RiShoppingCart2Line />
+                  </div>
+                </div>
+              </Link>
+            </SwiperSlide>
+          ))}
+        </Swiper>
+      </div>
+    </div>
+  );
 };
 
 export default RelatedProducts;
