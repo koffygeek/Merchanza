bdef763ae480a77fc1781c9cef6099147bfc0ce3
Koffi GREBE <yvan.grebe@email.com>
2024-06-14 03:59:11 +0100

    fin Item.jsx et modif PopularProducts.jsx

---
+++ frontend/src/components/Item.jsx
@@ -0,0 +1,32 @@
+import React from "react";
+import { Link } from "react-router-dom";
+import { RiShoppingCart2Line } from "react-icons/ri";
+
+const item = ({ id, name, image, old_price, new_price }) => {
+  return (
+    <Link to={""} className=" bg-white rounded-xl p-4 relative">
+      <div className="flexCenter">
+        <img
+          src={image}
+          alt="image"
+          height={"211px"}
+          width={"211px"}
+          className="rounded-lg drop-shadow-xl absolute bottom-44"
+        />
+      </div>
+      <div className="flex flex-col gap-y-4 pt-24">
+        <h4 className="line-clamp-2 medium-16">{name}</h4>
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quae.</p>
+        <div className="flexBetween">
+          <div className="flex gap-x-4 medium-16">
+            <span>${new_price}.00</span>
+            <span className="line-through text-secondary">${old_price}.00</span>
+          </div>
+          <RiShoppingCart2Line className="p-2 h-10 w-10 hover:text-secondary" />
+        </div>
+      </div>
+    </Link>
+  );
+};
+
+export default item;
--- frontend/src/components/PopularProducts.jsx
+++ frontend/src/components/PopularProducts.jsx
@@ -1,12 +1,13 @@
 import React from "react";
 import POPULAR from "../assets/popular";
+import Item from "./Item";
 
 const PopularProducts = () => {
   return (
-    <section>
+    <section className="max-padd-container bg-primary p-12 xl:py-28">
       {/* {title} */}
-      <div>
-        <h3>Popular Products</h3>
+      <div className="text-center max-w-xl mx-auto">
+        <h3 className="h3">Popular Products</h3>
         <p>
           Lorem ipsum dolor, sit amet consectetur adipisicing elit. Aliquam quae
           doloribus vero dolores adipisci quaerat tempora dolorem, modi porro
@@ -15,7 +16,18 @@ const PopularProducts = () => {
         </p>
       </div>
       {/* {container} */}
-      <div>{POPULAR}</div>
+      <div className="grid grid-cols-1 xs:grid-cols-2 lg:grid-cols-4 gap-6 gap-y-28 mt-32">
+        {POPULAR.map((item) => (
+          <Item
+            key={item.id}
+            id={item.id}
+            name={item.name}
+            image={item.image}
+            new_price={item.new_price}
+            old_price={item.old_price}
+          />
+        ))}
+      </div>
     </section>
   );
 };
