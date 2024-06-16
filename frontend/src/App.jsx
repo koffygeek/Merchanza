5155ec54d1441b40054e5d72fa8d0fc33a31b2bc
Koffi GREBE <yvan.grebe@email.com>
2024-06-16 09:02:36 +0100

    end category.jsx

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -4,6 +4,9 @@ import Home from "./pages/Home.jsx";
 import Category from "./pages/Category.jsx";
 import Product from "./pages/Product.jsx";
 import Footer from "./components/Footer.jsx";
+import clothingbanner from "./assets/clothingbanner.png";
+import cosmeticsbanner from "./assets/cosmeticsbanner.png";
+import electronicsbanner from "./assets/electronicsbanner.png";
 
 export default function App() {
   return (
@@ -12,10 +15,23 @@ export default function App() {
         <Header />
         <Routes>
           <Route path="/" element={<Home />} />
-          <Route path="/clothing" element={<Category />} />
-          <Route path="/cosmetics" element={<Category />} />
-          <Route path="/electronics" element={<Category />} />
-          <Route path="/product" element={<Category />} />
+          <Route
+            path="/clothing"
+            element={<Category category={"clothing"} banner={clothingbanner} />}
+          />
+          <Route
+            path="/cosmetics"
+            element={
+              <Category category={"cosmetics"} banner={cosmeticsbanner} />
+            }
+          />
+          <Route
+            path="/electronics"
+            element={
+              <Category category={"electronics"} banner={electronicsbanner} />
+            }
+          />
+          <Route path="/product" element={<Product />} />
           <Route path=":productId" element={<Product />}></Route>
           {/* <Route path="/cart-page" element={<Cart />} />
           <Route path="/login" element={<Login />} /> */}
