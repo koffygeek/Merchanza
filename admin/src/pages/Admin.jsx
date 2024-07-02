f131c5737db7417149c833a7568d741845c1495c
Koffi GREBE <yvan.grebe@email.com>
2024-07-02 18:08:23 +0100

    creation des components et ds pages

--- admin/src/pages/Admin.jsx
+++ admin/src/pages/Admin.jsx
@@ -1,9 +1,15 @@
 import React from "react";
+import SideBar from "../components/SideBar";
+import { Route, Routes } from "react-router-dom";
+import AddProduct from "../components/AddProduct";
 
 const Admin = () => {
   return (
     <div>
       <SideBar />
+      <Routes>
+        <Route path="/addproduct" element={<AddProduct />} />
+      </Routes>
     </div>
   );
 };
