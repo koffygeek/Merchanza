92a811e66e89dc3193b223b30c24da769aecbe3b
Koffi GREBE <yvan.grebe@email.com>
2024-07-04 11:13:30 +0100

    Creating endpoint for registering user

--- backend/index.js
+++ backend/index.js
@@ -154,6 +154,34 @@ const User = mongoose.model("User", {
 });
 
 // Creating endpoint for registering user
+app.post("/signup", async (req, res) => {
+  let check = await User.findOne({ email: req.body.email });
+  if (check) {
+    return res.status(400).json({
+      success: false,
+      errors: "Existing user found with same email",
+    });
+  }
+  let cart = {};
+  for (let i = 0; i < 300; i++) {
+    cart[i] = 0;
+  }
+  const user = new User({
+    name: req.body.name,
+    email: req.body.email,
+    password: req.body.password,
+    cartData: cart,
+  });
+
+  await user.save();
+  const data = {
+    user: {
+      id: user.id,
+    },
+  };
+  const token = jwt.sign(data, "secret_ecom");
+  res.json({ success: true, token });
+});
 
 app.listen(port, (error) => {
   if (!error) {
