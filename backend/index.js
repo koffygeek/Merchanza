21078fe15f976db136f53fc8f3cf286686bd891d
Koffi GREBE <yvan.grebe@email.com>
2024-07-10 11:13:28 +0100

    Creating middleware to fetch user

--- backend/index.js
+++ backend/index.js
@@ -216,6 +216,27 @@ app.get("/popularproducts", async (req, res) => {
   res.send(popularproducts);
 });
 
+// Creating middleware to fetch user
+const fetchUser = async (req, res, next) => {
+  const token = req.header("auth-token");
+  if (!token) {
+    res.status(401).send({ errors: "Please authenticate using valid login" });
+  } else {
+    try {
+      const data = jwt.verify(token, "secret_ecom");
+      req.user = data.user;
+      next();
+    } catch (error) {
+      res.status(401).send({ errors: "Please authenticate using valid token" });
+    }
+  }
+};
+
+// Creating endpoint for adding products in cartData
+app.post("/addtocart", async (req, res) => {
+  console.log(req.body);
+});
+
 app.listen(port, (error) => {
   if (!error) {
     console.log("Server is running on Port:  " + port);
